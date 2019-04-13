7c4855d06c6345c6c686543edfd899529b497e39
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 21:09:11 +0700

    set and get method

--- index.php
+++ index.php
@@ -4,22 +4,35 @@
 class testing{
 
   // property
-  public $text = 'Welcome to the OOP';
-  public $int = 123;
+  // public $text = 'Welcome to the OOP';
+  // public $int = 123;
+  public $text;
+  public $int;
 
   //metode
-  public function home(){
-    echo $this->text;
+  // public function home(){
+  //   echo $this->text;
+  // }
+  //
+  // public function count(){
+  //   return $this->int;
+  // }
+
+  // metode set and metode get
+  public function set_bro($param){
+    $this->text = $param;
   }
 
-  public function count(){
-    return $this->int;
+  public function get_bro(){
+    return $this->text;
   }
 
 }
 
 $hello = new testing;
 // echo $hello->text." ".$hello->int;
-$hello->home();
-echo $hello->count();
+// $hello->home();
+// echo $hello->count();
+$hello->set_bro('This is set and get method');
+echo $hello->get_bro();
 ?>
