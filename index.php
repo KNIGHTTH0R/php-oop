97c7b012c0d69fb8c236a7916b7a7b8bc1634439
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 21:02:12 +0700

    method

--- index.php
+++ index.php
@@ -6,9 +6,20 @@ class testing{
   // property
   public $text = 'Welcome to the OOP';
   public $int = 123;
+
+  //metode
+  public function home(){
+    echo $this->text;
+  }
+
+  public function count(){
+    return $this->int;
+  }
+
 }
 
 $hello = new testing;
-echo $hello->text." ".$hello->int;
-
+// echo $hello->text." ".$hello->int;
+$hello->home();
+echo $hello->count();
 ?>
