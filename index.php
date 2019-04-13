cc7719bd9129aaf05028f039dec96b90aa4f5119
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 20:57:47 +0700

    class and property

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// class
+class testing{
+
+  // property
+  public $text = 'Welcome to the OOP';
+  public $int = 123;
+}
+
+$hello = new testing;
+echo $hello->text." ".$hello->int;
+
+?>
