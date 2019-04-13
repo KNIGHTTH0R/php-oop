78eeb8b96037564143b25fe34ec0a293e9a9f38d
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 21:13:27 +0700

    construct

--- index.php
+++ index.php
@@ -1,33 +1,6 @@
 <?php
 
-// class
-class testing{
-
-  // property
-  // public $text = 'Welcome to the OOP';
-  // public $int = 123;
-  public $text;
-  public $int;
-
-  //metode
-  // public function home(){
-  //   echo $this->text;
-  // }
-  //
-  // public function count(){
-  //   return $this->int;
-  // }
-
-  // metode set and metode get
-  public function set_bro($param){
-    $this->text = $param;
-  }
-
-  public function get_bro(){
-    return $this->text;
-  }
-
-}
+require_once 'testing.php';
 
 $hello = new testing;
 // echo $hello->text." ".$hello->int;
---
+++ testing.php
@@ -0,0 +1,37 @@
+<?php
+
+// class
+class testing{
+
+  // property
+  // public $text = 'Welcome to the OOP';
+  // public $int = 123;
+  public $text;
+  public $int;
+
+  //metode
+  // public function home(){
+  //   echo $this->text;
+  // }
+  //
+  // public function count(){
+  //   return $this->int;
+  // }
+
+  //constructor
+  public function __construct(){
+    echo 'This is come from construct<br>';
+  }
+
+  // metode set and metode get
+  public function set_bro($param){
+    $this->text = $param;
+  }
+
+  public function get_bro(){
+    return $this->text;
+  }
+
+}
+
+?>
